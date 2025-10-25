66a86975abd280b51243150d53cbe122adcb2105
Adem <ado.designs@outlook.de>
2025-10-25 16:53:51 +0200

    feat: drf test models api_view with try-except block some statuscode

--- market_app/models.py
+++ market_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Market(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    net_worth = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=100)
+    markets = models.ManyToManyField(Market, related_name='sellers')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    market = models.ForeignKey(Market, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
